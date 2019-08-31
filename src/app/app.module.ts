df6df0ef64599a14a28ed5a08cbe63ac7fbcec90
suntansoker123 <sayantanthepointer@gmail.com>
2019-08-31 15:21:30 +0530

    updated proj2

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { ProdComponent } from './prod.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  declarations: [
+    AppComponent, ProdComponent
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
